61659f9e47e87c2d4a6c422acf382320f9d1441b
Zoe_guo2016 <Zoe_guo2016@MacdeiMac-2.local>
2017-10-29 23:10:04 +0800

    add crud function and form partial

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,51 @@
 class GroupsController < ApplicationController
+
+
   def index
     @groups = Group.all
   end
+
+  def show
+    @group = Group.find(params[:id])
+  end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to groups_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:id])
+    @group.update(group_params)
+    redirect_to groups_path, notice: 'update successfully'
+  end
+
+  def destroy
+    @group = Group.find(params[:id])
+    @group.destroy
+
+    redirect_to groups_path, notice: 'deleted successfully'
+  end
+
+
+
+  private
+
+  def group_params
+    params.require(:group).permit(:title, :description)
+  end
+
+
 end
