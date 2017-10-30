a84cd2c5c18f0fbac5df1fd9977060d7a155c629
Zoe_guo2016 <Zoe_guo2016@MacdeiMac-2.local>
2017-10-30 09:42:46 +0800

    create posts model&controller

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,5 @@
 class Group < ApplicationRecord
   validates :title, presence: true
   belongs_to :user
+  has_many :posts
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :groups       
+  has_many :groups
+  has_many :posts    
 end
